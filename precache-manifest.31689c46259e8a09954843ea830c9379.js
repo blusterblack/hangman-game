14b9839a1674ae82590bcfa9a20c82c356b66843
blusterblack <lethanhbinh010309@gmail.com>
2020-05-03 00:20:58 +0700

    Updates

---
+++ precache-manifest.31689c46259e8a09954843ea830c9379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27697188062fbf97688111cf0983d4b",
+    "url": "/hangman-game/index.html"
+  },
+  {
+    "revision": "258d4eedc7c5bd59c0e1",
+    "url": "/hangman-game/static/css/main.7b5e2b9c.chunk.css"
+  },
+  {
+    "revision": "bee78d6823833ce989bb",
+    "url": "/hangman-game/static/js/2.cf4a957e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-game/static/js/2.cf4a957e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258d4eedc7c5bd59c0e1",
+    "url": "/hangman-game/static/js/main.9718743c.chunk.js"
+  },
+  {
+    "revision": "5d50adaaf2cbe9aac476",
+    "url": "/hangman-game/static/js/runtime-main.b9e02cbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hangman-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
